3c296f7572995eb26a21e6af5600f94537dc447b
danilociaffi <45684439+danilociaffi@users.noreply.github.com>
2020-04-09 16:27:21 +0200

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -8,7 +8,7 @@ function get_num_files {
 
 num_files=$(get_num_files)
 
-echo "Enter a number to guess how many files are in this repo:"
+echo "Enter a number to guess how many files are in this directory:"
 read guess
 
 while [[ $guess -ne $num_files ]] 
